f845e3335b1f078b4e14fee48dda957423f4f9ef
Sean McIntyre <s.mcintyre@xverba.ca>
2015-11-24 23:51:56 -0500

    Implemented 1.4.1 version of container

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "spark.master  spark://$HOSTNAME:7077" > /opt/spark-1.5.2-bin-hadoop2.6/conf/spark-defaults.conf
+echo "spark.master  spark://$HOSTNAME:7077" > /opt/spark-1.4.1-bin-hadoop2.6/conf/spark-defaults.conf
 service ssh restart
-/opt/spark-1.5.2-bin-hadoop2.6/sbin/start-master.sh
-SPARK_WORKER_INSTANCES=2 /opt/spark-1.5.2-bin-hadoop2.6/sbin/start-slave.sh spark://$HOSTNAME:7077
+/opt/spark-1.4.1-bin-hadoop2.6/sbin/start-master.sh
+SPARK_WORKER_INSTANCES=2 /opt/spark-1.4.1-bin-hadoop2.6/sbin/start-slave.sh spark://$HOSTNAME:7077
 eval $1
