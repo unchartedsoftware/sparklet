c6b676847dff99e92433c00a1161dd80937b5c26
Sean McIntyre <s.mcintyre@xverba.ca>
2015-12-04 15:15:52 -0500

    Fixing bug in startup script.

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@ echo "spark.master  spark://$HOSTNAME:7077" > /opt/spark-1.5.2-bin-hadoop2.6/con
 service ssh restart
 /opt/spark-1.5.2-bin-hadoop2.6/sbin/start-master.sh
 SPARK_WORKER_INSTANCES=2 /opt/spark-1.5.2-bin-hadoop2.6/sbin/start-slave.sh spark://$HOSTNAME:7077
-eval $1
+eval $@
