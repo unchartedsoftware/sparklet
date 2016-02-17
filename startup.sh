675fe79d32b772a286b433091c7486a6f77313d7
Sean McIntyre <s.mcintyre@xverba.ca>
2016-02-17 14:48:07 -0500

    Locking worker memory to 1GB

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@
 echo "spark.master  spark://$HOSTNAME:7077" > /opt/spark-1.5.2-bin-hadoop2.6/conf/spark-defaults.conf
 service ssh restart
 /opt/spark-1.5.2-bin-hadoop2.6/sbin/start-master.sh
-SPARK_WORKER_INSTANCES=2 /opt/spark-1.5.2-bin-hadoop2.6/sbin/start-slave.sh spark://$HOSTNAME:7077
+SPARK_WORKER_INSTANCES=2 SPARK_WORKER_CORES=1 SPARK_WORKER_MEMORY=1g /opt/spark-1.5.2-bin-hadoop2.6/sbin/start-slave.sh spark://$HOSTNAME:7077
 eval $@
